ccd56a35cf46feeb85d4b81a7e4d66baafe7fe7b
Alexandr Barenboim <skivus@gmail.com>
2013-08-03 07:10:41 +0300

    new files added

---
+++ ImageWrapperDemo/ImageWrapperDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageWrapperDemo
+//
+//  Created by Alexandr Barenboim on 03.08.13.
+//  Copyright (c) 2013 Alexandr Barenboym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
